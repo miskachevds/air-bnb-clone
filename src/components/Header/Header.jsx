d4b140a4e65f1a048a1c98e4a7cecafd9474bd55
Miskachev Dima <miskachevds@yandex.ru>
2026-01-15 16:46:02 +0300

    searchbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import './Header.scss';
 
-function Header (){
-    return(
+function Header() {
+    return (
         <header className="header">
 
+            <div className="container">
+                <div className="header-row">
+                    <div className="header_logo">LOGO</div>
+                    <div className="header_search">SEARCH</div>
+                    <div className="header_user">USER MENU</div>
+                </div>
+            </div>
 
-
-            
         </header>
     )
 }
